8198fc68ddb7f083e24375687f5923673a483673
Mozhgan Ahsant <mozhganahsant@gmail.com>
2023-10-27 20:50:29 -0700

    login authorization done

--- application/Server/services/users.js
+++ application/Server/services/users.js
@@ -64,6 +64,48 @@ async function updateUser(user_id, user){
   return {message};
 }
 
+// for comparing the given password for login
+async function loginUser(user_email, user_password){
+
+  // get user_id from database, table profile
+  const result1 = await db.query(
+    `SELECT User_ID
+    FROM profile
+    WHERE Email=${user_email}`
+  );
+  if (!result1.affectedRows) {
+    let message = 'Error in loading User_ID from table profile';
+    return message;
+  }
+
+  // check if user_id is unique
+  if (result1.values.length != 1) {
+    let message = 'User_ID is not unique';
+    return message;
+  }
+
+  // get password from database, table registered_user
+  const result2 = await db.query(
+    `SELECT Password
+    FROM registered_user
+    WHERE User_ID=${result1.values[0].User_ID}]}]}`
+  );
+  if (!result2.affectedRows) {
+    let message = 'Error in loading Password from table registered_user';
+    return message;
+  }
+
+  // compare given password with databased password
+  const match = await bcrypt.compare(user_password, result2.values[0].password);
+  let message = "";
+  if (match) {
+    message = 'User login successfully';
+  } else {
+    message = 'Error in login user';
+  }
+
+  return {message};
+}
 
 // for deleting a user
 async function removeUser(user_id){
@@ -85,6 +127,7 @@ async function removeUser(user_id){
     getUsers,
     updateUser,
     removeUser, 
+    loginUser, 
   
   }
 
