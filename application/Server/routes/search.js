a1816a8ceef221df3a1dc674304a60314fc814f4
Aman Khera <aman.khera@outlook.com>
2023-11-28 00:25:54 -0800

    Backend updates for Search API

--- application/Server/routes/search.js
+++ application/Server/routes/search.js
@@ -8,8 +8,8 @@ module.exports = router;
   //for searching Listings
     router.get('/', async function(req, res, next) {
     try {
-        // console.log ("Rooms in Router = " + req.query.Rooms)
-      res.json(await search.searchListing(req.query.Rooms, req.query.Bathrooms, req.query.Price, req.query.Property_Type));
+        console.log ("Rooms in Router = " + req.query.Rooms + "The Min and Max Price is "+ req.query.Min_Price + " " + req.query.Max_Price )
+        res.json(await search.searchListing(req.query.Rooms, req.query.Bathrooms, req.query.Min_Price, req.query.Max_Price, req.query.Property_Type));
     } catch (err) {
       console.error(`Error while searching rental listings`, err.message);
       next(err);
--- application/Server/services/search.js
+++ application/Server/services/search.js
@@ -3,15 +3,15 @@ const helper = require('../helper');
 const config = require('../config');
 
 // Creating a function to search for listing depending on a number of parameters given by user
-async function searchListing(Rooms,Bathrooms, Price, Property_Type){
+async function searchListing(Rooms,Bathrooms, Min_Price, Max_Price, Property_Type){
     page=1
     const offset = helper.getOffset(page, config.listPerPage);
-    // console.log ("Rooms = " + Rooms + "Bathrooms = " +Bathrooms + "Price " + Price + "Property_Type "+ Property_Type)
+    console.log ("Rooms = " + Rooms + "Bathrooms = " +Bathrooms +", MinPrice "+ Min_Price+ ", MaxPrice " + Max_Price + ", Property_Type "+ Property_Type)
     // Building a search query to read from Rental_Listing and search for user inputted filters 
     const rows = await db.query(
     
       `SELECT * FROM Rental_Listing
-      WHERE Rooms >= ${Rooms} AND Bathrooms >= ${Bathrooms} AND Price <= ${Price} AND Property_Type = "${Property_Type}"`
+      WHERE Rooms >= ${Rooms} AND Bathrooms >= ${Bathrooms} AND Price >= ${Min_Price} AND Price <= ${Max_Price} AND Property_Type = "${Property_Type}"`
 
     
     )
